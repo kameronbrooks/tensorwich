dbd2208fb7e3789ecd38e435ec062589853facc2
kameronbrooks <kameron.cw.11@gmail.com>
2025-02-05 10:07:58 -0500

    - created basic TensorwichWrapper layout

--- tensorwich/core.py
+++ tensorwich/core.py
@@ -1,14 +1,21 @@
-class TensorWich:
-    def add(self, a, b):
-        return a + b
+import pandas as pd
+import numpy as np
 
-    def subtract(self, a, b):
-        return a - b
+class TensorwichWrapper:
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+        self.embeddings = None
+    
+    def save_file(self, path:str, *, embedding_column_name=None):
+        # TODO : Implement save function
+        pass
 
-    def multiply(self, a, b):
-        return a * b
+    def generate_embeddings(self, target_column:str, *, model:any=None, embedding_func:callable=None, column_name:str=None):
+        # TODO : Implement generate_embeddings function
+        pass
 
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Cannot divide by zero.")
-        return a / b
+    def load_file(self, path:str, *, embedding_column_name='embedding'):
+        # TODO : Implement load function
+        pass
+
+    
